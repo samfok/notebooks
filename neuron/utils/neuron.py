db985d5e717eea25e43516e5acb53547ab24bb72
Sam Fok <sam.b.fok@gmail.com>
2014-12-12 00:19:11 -0800

    pull sim_fi into utils

--- neuron/utils/neuron.py
+++ neuron/utils/neuron.py
@@ -1,7 +1,9 @@
 # define neuron models
 import numpy as np
 
-
+###############################################################################
+# theoretical and theoretical approximations of input, firing rate relations ##
+###############################################################################
 def th_lif_fi(u, tau, tref, xt):
     """Theoretical LIF tuning curve
 
@@ -107,9 +109,12 @@ def th_lif_if(f, tau, tref, xt):
     return u
 
 
-def num_alif_fi(u, tau, tref, xt, af=1e-3, max_iter=100, rel_tol=1e-3,
-                 verbose=False):
-    """Numerically find rate-based adaptive LIF tuning curve
+###############################################################################
+# numerical methods for determining input, firing rate relations ##############
+###############################################################################
+def num_alif_fi(u, tau, tref, xt, af=1e-3, tauf=1e-2, ref_fdecay=True,
+                max_iter=100, rel_tol=1e-3, verbose=False):
+    """Numerically determine the adaptive LIF neuron tuning curve
 
     Uses binary search to find the steady state firing rate of an adaptive LIF
     neuron.
@@ -126,14 +131,22 @@ def num_alif_fi(u, tau, tref, xt, af=1e-3, max_iter=100, rel_tol=1e-3,
         threshold
     af : float (optional)
         scales the inhibitory feedback
+    tauf : float (optional)
+        time constant of the feedback synapse
     max_iter : int (optional)
         maximium number of iterations in binary search
     rel_tol : float (optional)
-        relative tolerance of binary search algorithm. algorithm terminates
+        relative tolerance of binary search algorithm. The algorithm terminates
         when maximum difference between estimated af and input af is within
         tol*af
+    ref_fdecay : boolean (optional)
+        whether the feedback decays during the refractory period. If the
+        feedback decays during the refractory period, af is scaled by 
+        exp(-tref/tauf)
     """
     assert af > 0, "inhibitory feedback scaling must be > 0"
+    if ref_fdecay:
+        af *= np.exp(-tref/tauf)
     if isinstance(u, (int, float)):  # handle scalars
         u = np.array([u])
 
@@ -167,6 +180,91 @@ def num_alif_fi(u, tau, tref, xt, af=1e-3, max_iter=100, rel_tol=1e-3,
     return f_ret
 
 
+###############################################################################
+# empirical methods for determining input, firing rate relations ##############
+###############################################################################
+def sim_lif_fi(dt, u, tau, tref, xt):
+    """Find the LIF tuning curve by simulating the neuron
+
+    Parameters
+    ----------
+    u : array-like of floats
+        input
+    tau : float
+        membrane time constant
+    tref : float
+        refractory period
+    xt : float
+        threshold
+    """
+    # theory used to set how long to simulate
+    th_f = th_lif_fi(u, tau, tref, xt)
+    sim_f = np.zeros(th_af.shape)
+    for idx, u_val in enumerate(u):
+        if th_f[idx] < .01:
+            # estimated firing rate too low. would require too long to simulate
+            continue
+        T_f = 1./th_f[idx]  # expected interspike interval
+        # run long enough to collect some spikes
+        run_time = 5.*T_af
+        u_in = u_val+np.zeros(int(np.ceil(run_time/dt)))
+        spike_times = run_lifsoma(dt, u_in, tau_m, tref, xt, af, tauf)
+        isi = np.diff(spike_times[-3:])
+        if (isi[-2]-isi[-1])/isi[-2] > .01:
+            print('Warning (sim_lif_fi): ' +
+                  'Greater than 1% change in isi between last two isi. ' +
+                  'Something is wrong for u=%.2f...' % u_val)
+        sim_f[idx] = 1./isi[-1]
+    return sim_f
+
+
+def sim_alif_fi(dt, u, taum, tref, xt, af=1e-3, tauf=1e-2,
+                ref_fdecay=True):
+    """Find the adaptive LIF tuning curve by simulating the neuron
+
+    Parameters
+    ----------
+    u : array-like of floats
+        input
+    taum : float
+        membrane time constant
+    tref : float
+        refractory period
+    xt : float
+        threshold
+    af : float (optional)
+        scales the inhibitory feedback
+    tauf : float (optional)
+        time constant of the feedback synapse
+    ref_fdecay : boolean (optional)
+        whether the feedback decays during the refractory period. If the
+        feedback decays during the refractory period, af is scaled by 
+        exp(-tref/tauf)
+    """
+    # numerical estimate used to set how long to simulate
+    num_af = num_alif_fi(u, taum, tref, xt, af, tauf, ref_fdecay)
+    sim_af = np.zeros(num_af.shape)
+    for idx, u_val in enumerate(u):
+        if num_af[idx] < .01:
+            # estimated firing rate too low. would require too long to simulate
+            continue
+        T_af = 1./num_af[idx]  # expected interspike interval
+        # run long enough to reach steady state and collect some spikes
+        run_time = 5.*tauf+5.*T_af
+        u_in = u_val+np.zeros(int(np.ceil(run_time/dt)))
+        spike_times = run_alifsoma(dt, u_in, taum, tref, xt, af, tauf)
+        isi = np.diff(spike_times[-3:])
+        if (isi[-2]-isi[-1])/isi[-2] > .01:
+            print('Warning (sim_alif_fi): ' +
+                  'Greater than 1% change in isi between last two isi. ' +
+                  'Probably has not reached steady state for u_in=%.2f' % u_val)
+        sim_af[idx] = 1./isi[-1]
+    return sim_af
+
+
+###############################################################################
+# methods for running neuron models ###########################################
+###############################################################################
 def run_lifsoma(dt, u, tau, tref, xt, ret_state=False, flatten1=True):
     """Simulates an LIF soma(s) given an input current
 
@@ -241,7 +339,7 @@ def run_lifsoma(dt, u, tau, tref, xt, ret_state=False, flatten1=True):
     return retval
 
 
-def run_alifsoma(dt, u, tau, tref, xt, af=1e-2, tauf=1e-3,
+def run_alifsoma(dt, u, tau, tref, xt, af=1e-2, tauf=1e-2,
                  ret_state=False, ret_fstate=False, flatten1=True):
     """Simulates an adaptive LIF soma(s) given an input current
 
